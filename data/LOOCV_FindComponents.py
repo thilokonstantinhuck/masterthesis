8db50a2826fd52c2c4ea45672b4b5c41520f6074
thilohuc <thilo.huck@nmbu.no>
2024-11-26 13:47:55 +0100

    llocv working, start work on new dataset

--- data/LOOCV_FindComponents.py
+++ data/LOOCV_FindComponents.py
@@ -7,7 +7,8 @@ from sklearn.cross_decomposition import PLSRegression
 from sklearn.metrics import r2_score
 
 ### Settings
-target = "C18:2n6c"
+# target = "C18:1n9t"
+target = "EPAandDHA"
 # target = "Lipid_%"
 # Components range to graph and calculate
 compFirst = 1
@@ -22,15 +23,18 @@ samples = ["S01", "S02", "S03", "S04", "S05", "S06", "S07", "S08", "S09", "S10",
 file_path = 'exported_data_coarse_median.csv'
 data = pd.read_csv(file_path)
 
-r2_score_list_components = []
+r2_score_list_Test = []
+r2_score_list_Train = []
 
 for i in range(compFirst, compLast + 1):
 
     # Model with specific number of components selected
     pls_model = PLSRegression(n_components=i)
 
-    predicted = []
-    actual = []
+    predictedTest = []
+    predictedTrain = []
+    actualTest = []
+    actualTrain = []
 
     for sample in samples:
         train_X = data[data["Fish ID"] != sample].iloc[:, firstWL:].values
@@ -41,26 +45,40 @@ for i in range(compFirst, compLast + 1):
         # Train the PLS model on the train set
         pls_model.fit(train_X, train_y)
 
-        # Predict on the test set
-        y_pred = pls_model.predict(test_X)
+        # Predict on both
+        y_pred_train = pls_model.predict(train_X)
+        y_pred_test = pls_model.predict(test_X)
 
-        predicted.extend(y_pred.flatten())  # Append the predicted values
-        actual.extend(test_y.flatten())  # Append the actual values
+        predictedTrain.extend(y_pred_train.flatten())  # Append the predicted values
+        actualTrain.extend(train_y.flatten())  # Append the actual values
 
-    predicted = np.array(predicted)
-    actual = np.array(actual)
+        predictedTest.extend(y_pred_test.flatten())  # Append the predicted values
+        actualTest.extend(test_y.flatten())  # Append the actual values
 
-    r2 = r2_score(actual, predicted)
-    r2_score_list_components.append(r2)
+    predictedTest = np.array(predictedTest)
+    actualTest = np.array(actualTest)
 
-    print(f"R² SCORE for {i} Components: {r2}")
+    r2test = r2_score(actualTest, predictedTest)
+    r2_score_list_Test.append(r2test)
+
+    predictedTrain = np.array(predictedTrain)
+    actualTrain = np.array(actualTrain)
+
+    r2train = r2_score(actualTrain, predictedTrain)
+    r2_score_list_Train.append(r2train)
+
+    print(f"R² SCORE for {i} Components: {r2test}")
 
 # Plot the R² scores for each number of components
 plt.figure(figsize=(10, 6))
-plt.plot(range(compFirst, compLast + 1), r2_score_list_components, marker='o', linestyle='-', color='b')
+plt.plot(range(compFirst, compLast + 1), r2_score_list_Test, marker='o', linestyle='-', color='b', label="Test")
+plt.plot(range(compFirst, compLast + 1), r2_score_list_Train, marker='o', linestyle='-', color='r', label="Train")
 plt.xlabel('Number of Components')
 plt.ylabel('R² Score')
 plt.title(f'R² Score vs Number of Components in PLS Model for {target} using LOOCV')
 plt.xticks(range(compFirst, compLast + 1))  # Ensure each component is marked on the x-axis
 plt.grid(True)
+plt.ylim(0,1)
+plt.yticks(np.arange(0, 1.1, 0.1))
+plt.legend()
 plt.show()
