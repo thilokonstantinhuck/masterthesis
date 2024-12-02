a328aabe843424ad437ecb251f0325f03faa904e
thilohuc <thilo.huck@nmbu.no>
2024-12-02 15:56:58 +0100

    update for F1

--- data/LOOCV_FindComponents.py
+++ data/LOOCV_FindComponents.py
@@ -5,9 +5,18 @@ from sklearn.cross_decomposition import PLSRegression
 from sklearn.metrics import r2_score
 
 ### Settings
-# target = "C20:1n9"
-# target = "EPAandDHA"
-target = "Lipid_%"
+list_targets = ['Lipid_%',
+                'Saturated', #1
+                'Unsaturated', #2
+                'Monounsaturated', #3
+                'Polyunsaturated', #4
+                'Omega9', #5
+                'Omega6', #6
+                'Omega3', #7
+                'HumanOmega3', #8
+                'EPAandDHA', #9
+                'C4:0', 'C6:0', 'C8:0', 'C10:0', 'C11:0', 'C12:0', 'C14:0', 'C14:1n5', 'C15:0', 'C15:1n5', 'C16:0', 'C16:1n7', 'C17:0', 'C17:1n7', 'C18:0', 'C18:1n9c', 'C18:1n7', 'C18:2n6c', 'C18:2n6t', 'C18:3n3c', 'C18:3n6c', 'C20:0', 'C20:1n9', 'C20:2n6', 'C20:3n3', 'C20:3n6', 'C20:4n6', 'C20:5n3', 'C21:0', 'C22:0', 'C22:1n9', 'C22:2n6', 'C22:5n3', 'C22:6n3', 'C23:0', 'C24:0', 'C24:1n9']
+target = list_targets[4] # 46 max
 # Components range to graph and calculate
 compFirst = 1
 compLast = 25
@@ -23,7 +32,7 @@ data_coarse = pd.read_csv(file_path)
 r2_score_list_test_coarse = []
 r2_score_list_train_coarse = []
 # Define where the first wavlength is located
-first_wavelength_coarse = 41
+first_wavelength_coarse = 49
 print(data_coarse.columns[first_wavelength_coarse])
 
 # all replicate fine
@@ -32,7 +41,7 @@ data_fine = pd.read_csv(file_path)
 r2_score_list_test_fine = []
 r2_score_list_train_fine = []
 # Define where the first wavlength is located
-first_wavelength_fine = 46
+first_wavelength_fine = 53
 print(data_fine.columns[first_wavelength_fine])
 
 for i in range(compFirst, compLast + 1):
@@ -46,10 +55,10 @@ for i in range(compFirst, compLast + 1):
     actualTrain = []
 
     for sample in samples:
-        train_X = data_coarse[data_coarse["Fish ID"] != sample].iloc[:, first_wavelength_coarse:].values
-        test_X = data_coarse[data_coarse["Fish ID"] == sample].iloc[:, first_wavelength_coarse:].values
-        train_y = data_coarse[data_coarse["Fish ID"] != sample][target].values
-        test_y = data_coarse[data_coarse["Fish ID"] == sample][target].values
+        train_X = data_coarse[data_coarse["Fish_ID"] != sample].iloc[:, first_wavelength_coarse:].values
+        test_X = data_coarse[data_coarse["Fish_ID"] == sample].iloc[:, first_wavelength_coarse:].values
+        train_y = data_coarse[data_coarse["Fish_ID"] != sample][target].values
+        test_y = data_coarse[data_coarse["Fish_ID"] == sample][target].values
 
         # Train the PLS model on the train set
         pls_model.fit(train_X, train_y)
@@ -89,10 +98,10 @@ for i in range(compFirst, compLast + 1):
     actualTrain = []
 
     for sample in samples:
-        train_X = data_fine[data_fine["Fish ID"] != sample].iloc[:, first_wavelength_fine:].values
-        test_X = data_coarse[data_coarse["Fish ID"] == sample].iloc[:, first_wavelength_coarse:].values
-        train_y = data_fine[data_fine["Fish ID"] != sample][target].values
-        test_y = data_coarse[data_coarse["Fish ID"] == sample][target].values
+        train_X = data_fine[data_fine["Fish_ID"] != sample].iloc[:, first_wavelength_fine:].values
+        test_X = data_coarse[data_coarse["Fish_ID"] == sample].iloc[:, first_wavelength_coarse:].values
+        train_y = data_fine[data_fine["Fish_ID"] != sample][target].values
+        test_y = data_coarse[data_coarse["Fish_ID"] == sample][target].values
 
         # Train the PLS model on the train set
         pls_model.fit(train_X, train_y)
--- data/predict image.py
+++ data/predict image.py
@@ -1,11 +1,21 @@
 import spectral.io.envi as envi
-import pickle
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from sklearn.cross_decomposition import PLSRegression
 import cv2
-import os
 
-# change here
+## Settings
+number_of_components = 5
+
+# Set grayscale limits depending on the target for the predicted image
+# target = "Lipid_%"
+# min_val, max_val = 0, 25  # Adjust these values as needed
+target = "EPAandDHA"
+min_val, max_val = 1, 15  # Adjust these values as needed
+
+
+datasetChoice = 3
 samplename = "S06"
 ROI = [
     [240, 750],
@@ -22,20 +32,36 @@ ROI = [
     [245, 756],
 ]
 
-# Load the trained PLS model
-model_filename = 'pls_model.pkl'
-with open(model_filename, 'rb') as file:
-    pls_model = pickle.load(file)
+##Dataset
 
-# Load the original image
-# filename = "S6-R-G1_SWIR_384_SN3151_9006us_2022-05-02T120753_raw_rad_float32.hdr"
-# imageFolder = r"C:\SALMONDATA\SWIR_Hyspex\All_converted_file"
-# hdr = os.path.join(imageFolder, filename)
+# median coarse
+file_path = f'exported_data_coarse_median_dataset{datasetChoice}.csv'
+data_coarse = pd.read_csv(file_path)
+# Define where the first wavlength is located
+first_wavelength_coarse = 41
+print(data_coarse.columns[first_wavelength_coarse])
+X_coarse = data_coarse.iloc[:, first_wavelength_coarse:].values
+y_coarse = data_coarse[target].values
 
-hdr = f"../tempImages/processed_image_{samplename}_absorbance_EMSC.hdr"
+# all replicate fine
+file_path = f'exported_data_fine_dataset{datasetChoice}.csv'
+data_fine = pd.read_csv(file_path)
+# Define where the first wavlength is located
+first_wavelength_fine = 46
+print(data_fine.columns[first_wavelength_fine])
+X_fine = data_fine.iloc[:, first_wavelength_fine:].values
+y_fine = data_fine[target].values
 
+
+hdr = f"../tempImages/{datasetChoice}_processed_image_{samplename}_absorbance_EMSC.hdr"
 img = envi.open(hdr)
 image = img.load()
+
+## Model Creation
+pls_model_fine = PLSRegression(n_components=number_of_components)
+pls_model_fine.fit(X_fine, y_fine)
+pls_model_coarse = PLSRegression(n_components=number_of_components)
+pls_model_coarse.fit(X_coarse, y_coarse)
 
 # Reshape the image data to be compatible with the model
 # The image is reshaped into (num_pixels, num_wavelengths)
@@ -44,46 +70,49 @@ num_wavelengths = image.shape[2]
 X = image.reshape((num_pixels, num_wavelengths))
 
 # Make predictions using the loaded model
-y_pred = pls_model.predict(X).flatten()
+y_pred_coarse = pls_model_coarse.predict(X).flatten()
+y_pred_fine = pls_model_fine.predict(X).flatten()
 
 # Reshape the predictions back to the original image shape (height, width)
-predicted_image = y_pred.reshape((image.shape[0], image.shape[1]))
+predicted_image_coarse = y_pred_coarse.reshape((image.shape[0], image.shape[1]))
+predicted_image_fine = y_pred_fine.reshape((image.shape[0], image.shape[1]))
 
-# Set grayscale limits for the predicted image
-min_val, max_val = 0, 30  # Adjust these values as needed
+
 
 # Scale the predicted image to the specified grayscale limits and save as PNG
-scaled_image = np.clip((predicted_image - min_val) * 255 / (max_val - min_val), 0, 255).astype(np.uint8)
-cv2.imwrite(f"predicted_image_{samplename}.png", scaled_image)
+predicted_image_coarse_scaled = np.clip((predicted_image_coarse - min_val) * 255 / (max_val - min_val), 0, 255).astype(np.uint8)
+predicted_image_fine_scaled = np.clip((predicted_image_fine - min_val) * 255 / (max_val - min_val), 0, 255).astype(np.uint8)
+cv2.imwrite(f"../plots/{datasetChoice}_{samplename}_{target}_{number_of_components}_coarse.png", predicted_image_coarse_scaled)
+cv2.imwrite(f"../plots/{datasetChoice}_{samplename}_{target}_{number_of_components}_fine.png", predicted_image_fine_scaled)
 
-# Retrieve the wavelengths from the header metadata
-wavelengths = np.array(img.metadata['wavelength'], dtype=np.float32).flatten()
-
-# Plot the predicted values as an image and the spectra of selected pixels
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 20), gridspec_kw={'width_ratios': [1, 2]})
-
-# Plot the predicted image
-cax = ax1.imshow(predicted_image, cmap='viridis', vmin=-1, vmax=30)
-fig.colorbar(cax, ax=ax1, label="Predicted %")
-ax1.set_title("Lipid Prediction")
-ax1.set_xlabel("Width")
-ax1.set_ylabel("Height")
-
-# Mark the selected ROI pixels in red
-for pixel in ROI:
-    ax1.plot(pixel[0], pixel[1], 'ro')
-
-# Plot the spectrum at the selected positions
-for pixel in ROI:
-    ax2.plot(wavelengths, image[pixel[1], pixel[0], :].flatten(), label=f"Pixel ({pixel[0]}, {pixel[1]}), Value of {round(predicted_image[pixel[1], pixel[0]],2)}")
-
-ax2.set_xlabel("Wavelength (nm)")
-ax2.set_ylabel("Absorbance")
-ax2.set_title("Spectra of Selected Pixels")
-#ax2.set_ylim([-0.01, 0.3])  # Set y-axis limits for ax2
-ax2.set_ylim([-0.1, 3])  # Set y-axis limits for ax2
-ax2.legend()
-ax2.grid(True)
-
-plt.tight_layout()
-plt.show()
+# # Retrieve the wavelengths from the header metadata
+# wavelengths = np.array(img.metadata['wavelength'], dtype=np.float32).flatten()
+#
+# # Plot the predicted values as an image and the spectra of selected pixels
+# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 20), gridspec_kw={'width_ratios': [1, 2]})
+#
+# # Plot the predicted image
+# cax = ax1.imshow(predicted_image, cmap='viridis', vmin=-1, vmax=30)
+# fig.colorbar(cax, ax=ax1, label="Predicted %")
+# ax1.set_title("Lipid Prediction")
+# ax1.set_xlabel("Width")
+# ax1.set_ylabel("Height")
+#
+# # Mark the selected ROI pixels in red
+# for pixel in ROI:
+#     ax1.plot(pixel[0], pixel[1], 'ro')
+#
+# # Plot the spectrum at the selected positions
+# for pixel in ROI:
+#     ax2.plot(wavelengths, image[pixel[1], pixel[0], :].flatten(), label=f"Pixel ({pixel[0]}, {pixel[1]}), Value of {round(predicted_image[pixel[1], pixel[0]],2)}")
+#
+# ax2.set_xlabel("Wavelength (nm)")
+# ax2.set_ylabel("Absorbance")
+# ax2.set_title("Spectra of Selected Pixels")
+# #ax2.set_ylim([-0.01, 0.3])  # Set y-axis limits for ax2
+# ax2.set_ylim([-0.1, 3])  # Set y-axis limits for ax2
+# ax2.legend()
+# ax2.grid(True)
+#
+# plt.tight_layout()
+# plt.show()
